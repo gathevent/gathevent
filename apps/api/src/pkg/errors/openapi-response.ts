858c21a5e629a2e42ce97c3a8d3e6aaf52a88168
Yaniv <yanivdouieb@icloud.com>
2025-10-02 22:21:27 +0200

    feat(api): update error response schema identifiers

--- apps/api/src/pkg/errors/openapi-response.ts
+++ apps/api/src/pkg/errors/openapi-response.ts
@@ -7,7 +7,7 @@ export const openApiErrorResponses = {
       "The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).",
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["BAD_REQUEST"])).openapi("ErrBadRequest"),
+        schema: errorSchemaFactory(z.enum(["BAD_REQUEST"])).openapi("ErrorBadRequest"),
       },
     },
   },
@@ -15,7 +15,7 @@ export const openApiErrorResponses = {
     description: `Although the HTTP standard specifies "unauthorized", semantically this response means "unauthenticated". That is, the client must authenticate itself to get the requested response.`,
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["UNAUTHORIZED"])).openapi("ErrUnauthorized"),
+        schema: errorSchemaFactory(z.enum(["UNAUTHORIZED"])).openapi("ErrorUnauthorized"),
       },
     },
   },
@@ -24,7 +24,7 @@ export const openApiErrorResponses = {
       "The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401 Unauthorized, the client's identity is known to the server.",
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["FORBIDDEN"])).openapi("ErrForbidden"),
+        schema: errorSchemaFactory(z.enum(["FORBIDDEN"])).openapi("ErrorForbidden"),
       },
     },
   },
@@ -33,7 +33,7 @@ export const openApiErrorResponses = {
       "The server cannot find the requested resource. In the browser, this means the URL is not recognized. In an API, this can also mean that the endpoint is valid but the resource itself does not exist. Servers may also send this response instead of 403 Forbidden to hide the existence of a resource from an unauthorized client. This response code is probably the most well known due to its frequent occurrence on the web.",
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["NOT_FOUND"])).openapi("ErrNotFound"),
+        schema: errorSchemaFactory(z.enum(["NOT_FOUND"])).openapi("ErrorNotFound"),
       },
     },
   },
@@ -41,7 +41,7 @@ export const openApiErrorResponses = {
     description: "The request method is not supported for the requested resource.",
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["METHOD_NOT_ALLOWED"])).openapi("ErrMethodNotAllowed"),
+        schema: errorSchemaFactory(z.enum(["METHOD_NOT_ALLOWED"])).openapi("ErrorMethodNotAllowed"),
       },
     },
   },
@@ -50,7 +50,7 @@ export const openApiErrorResponses = {
       "This response is sent when a request conflicts with the current state of the server.",
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["CONFLICT"])).openapi("ErrConflict"),
+        schema: errorSchemaFactory(z.enum(["CONFLICT"])).openapi("ErrorConflict"),
       },
     },
   },
@@ -58,7 +58,7 @@ export const openApiErrorResponses = {
     description: `The user has sent too many requests in a given amount of time ("rate limiting")`,
     content: {
       "application/json": {
-        schema: errorSchemaFactory(z.enum(["TOO_MANY_REQUESTS"])).openapi("ErrTooManyRequests"),
+        schema: errorSchemaFactory(z.enum(["TOO_MANY_REQUESTS"])).openapi("ErrorTooManyRequests"),
       },
     },
   },
@@ -67,7 +67,7 @@ export const openApiErrorResponses = {
     content: {
       "application/json": {
         schema: errorSchemaFactory(z.enum(["INTERNAL_SERVER_ERROR"])).openapi(
-          "ErrInternalServerError",
+          "ErrorInternalServerError",
         ),
       },
     },
